0c017e644af755f7286e7d9366ba8a7bfb9ee753
Mariano Campo <mcampo85@gmail.com>
2015-12-26 17:32:20 -0300

    add animation cycling every 30 secs

--- xmas-pixels.ino
+++ xmas-pixels.ino
@@ -8,6 +8,7 @@
 
 #define PIN 6
 #define BUTTON_PIN 2
+#define ANIMATION_CYCLE_INTERVAL 30000
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN);
 FadeAnimation fadeAnimation(&strip, 150, 150, 255);
@@ -16,9 +17,12 @@ SparksAnimation sparksAnimation(&strip);
 TheaterChaseAnimation theaterChaseAnimation(&strip);
 RainbowCycleAnimation rainbowCycleAnimation(&strip);
 NeoPixelAnimation *animations[] = {&rainbowAnimation, &fadeAnimation, &sparksAnimation, &theaterChaseAnimation, &rainbowCycleAnimation};
+byte animationCount = (sizeof(animations) / sizeof(NeoPixelAnimation *));
 
 bool buttonPreviousState = HIGH;
 byte animationIndex = 0;
+unsigned long timeForNextAnimation = 0;
+bool cycleAnimations = true;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -26,9 +30,14 @@ void setup() {
   strip.begin();
   strip.setBrightness(20);
   strip.show();
+
+  timeForNextAnimation = millis() + ANIMATION_CYCLE_INTERVAL;
 }
 
 void loop() {
+  if (cycleAnimations) {
+    checkTimeForNextAnimation();
+  }
   checkButton();
   animations[animationIndex]->nextFrame();
 }
@@ -41,13 +50,38 @@ void checkButton() {
     // Check if button is still low after debounce.
     buttonNewState = digitalRead(BUTTON_PIN);
     if (buttonNewState == LOW) {
-      animationIndex++;
-      if (animationIndex >= (sizeof(animations) / sizeof(NeoPixelAnimation *))) {
-        animationIndex = 0;        
-      }
-      animations[animationIndex]->reset();
+      buttonPressed();
+      delay(20);
     }
   }
   buttonPreviousState = buttonNewState;
+}
+
+void buttonPressed() {
+  if(cycleAnimations) {
+    cycleAnimations = false;
+    return;
+  }
+  if (animationIndex == animationCount - 1) { //last animation
+    cycleAnimations = true;
+    timeForNextAnimation = millis() + ANIMATION_CYCLE_INTERVAL;
+  }
+  nextAnimation();
+}
+
+void checkTimeForNextAnimation() {
+  unsigned long currentTime = millis();
+  if (currentTime >= timeForNextAnimation) {
+    nextAnimation();
+    timeForNextAnimation = millis() + ANIMATION_CYCLE_INTERVAL;
+  }
+}
+
+void nextAnimation() {
+  animationIndex++;
+  if (animationIndex >= animationCount) {
+    animationIndex = 0;
+  }
+  animations[animationIndex]->reset();
 }
 
